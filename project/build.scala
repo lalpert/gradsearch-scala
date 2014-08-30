7c935094e29d88a8f5abd7e2e33e053931b69d76
Russell Cohen <russell@rcoh-laptop.(none)>
2014-08-29 21:10:53 -0700

    First commit of scalatra project

---
+++ project/build.scala
@@ -0,0 +1,47 @@
+import sbt._
+import Keys._
+import org.scalatra.sbt._
+import org.scalatra.sbt.PluginKeys._
+import com.mojolly.scalate.ScalatePlugin._
+import ScalateKeys._
+
+object GradsearchBuild extends Build {
+  val Organization = "org.gradschoolsearch"
+  val Name = "gradsearch"
+  val Version = "0.1.0-SNAPSHOT"
+  val ScalaVersion = "2.11.1"
+  val ScalatraVersion = "2.3.0"
+
+  lazy val project = Project (
+    "gradsearch",
+    file("."),
+    settings = ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      scalaVersion := ScalaVersion,
+      resolvers += Classpaths.typesafeReleases,
+      libraryDependencies ++= Seq(
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.1.3.v20140225" % "container",
+        "org.eclipse.jetty" % "jetty-plus" % "9.1.3.v20140225" % "container",
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+      ),
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+        Seq(
+          TemplateConfig(
+            base / "webapp" / "WEB-INF" / "templates",
+            Seq.empty,  /* default imports should be added here */
+            Seq(
+              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+            ),  /* add extra bindings here */
+            Some("templates")
+          )
+        )
+      }
+    )
+  )
+}
