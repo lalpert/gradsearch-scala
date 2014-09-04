f679670b1b38f263115d6cea021982277ac28dfd
Russell Cohen <russell.r.cohen@gmail.com>
2014-09-03 22:40:25 -0700

    Add professor info from Brown, CMU, MIT and Penn. Update the loader to work with it.

--- src/main/scala/org/gradschoolsearch/db/DataLoader.scala
+++ src/main/scala/org/gradschoolsearch/db/DataLoader.scala
@@ -11,21 +11,25 @@ import org.json4s.jackson.JsonMethods._
 import scala.io.Source
 object DataLoader {
   case class ProfWithKeywords(professor: DBProfessor, keywords: List[String])
-  private case class InternalFormat(school: String, name: String, image: String, source: String,
-                                     keywords: List[String], department: String)
-  val dataFields = List("src/main/resources/data/yale.dat.json")//, "src/main/resources/data/brown.dat.json")
+  private case class InternalFormat(school: String, name: String, image: Option[String], source: Option[String],
+                                     keywords: List[String], department: Option[String])
+  val schools = List("yale", "mit", "cmu", "penn")
+  val dataFields = schools.map(school => f"src/main/resources/data/$school.dat.json")
   implicit val formats = DefaultFormats
   def loadData() = {
     println(new java.io.File(".").getAbsolutePath() + "hello2")
     dataFields.flatMap { fileName =>
       val file = Source.fromFile(fileName).mkString
       val json = parse(file)
-      println(file.take(20))
       val professorsInternal = json.extract[List[InternalFormat]]
-      professorsInternal.map(internal => ProfWithKeywords(
-        DBProfessor(None, internal.name, internal.school, internal.department),
-        internal.keywords
-      ))
+      professorsInternal.map(cleanData).toSet.toList
     }
+  }
+
+  def cleanData(incoming: InternalFormat): ProfWithKeywords = {
+     ProfWithKeywords(
+      DBProfessor(None, incoming.name, incoming.school, incoming.department.getOrElse("No Department Known")),
+        incoming.keywords.map(_.take(250))
+     )
   }
 }
