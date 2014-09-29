7509e60ea11992ff4f3fa0ab54e645519e0169de
Russell Cohen <russell.r.cohen@gmail.com>
2014-09-28 23:42:52 -0700

    HBS json

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./sbt package
 rsync ./target/scala-2.11/gradsearch_2.11-0.1.0-SNAPSHOT.war gradsearch@gradschoolsearch.org:ROOT.war
+rsync -r ./src/main/resources/data gradsearch@gradschoolsearch.org:data
