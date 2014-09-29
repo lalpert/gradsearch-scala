31197db929553638a18d222740c69c5d06d95700
Russell Cohen <russell.r.cohen@gmail.com>
2014-09-28 23:49:01 -0700

    update deploy to sync profs

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./sbt package
 rsync ./target/scala-2.11/gradsearch_2.11-0.1.0-SNAPSHOT.war gradsearch@gradschoolsearch.org:ROOT.war
-rsync -r ./src/main/resources/data gradsearch@gradschoolsearch.org:data
+rsync -r -L ./src/main/resources/data gradsearch@gradschoolsearch.org:data
